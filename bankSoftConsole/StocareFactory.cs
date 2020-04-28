e7f1ff76369d0b1c47ea2d302627c8033845cf03
KREN1 <30318710+krenfix15@users.noreply.github.com>
2020-04-28 13:45:35 +0300

    Add files via upload

---
+++ bankSoftConsole/StocareFactory.cs
@@ -0,0 +1,29 @@
+﻿using NivelAccesDate;
+using System.Configuration;
+
+namespace bankSoftConsole
+{
+    public static class StocareFactory
+    {
+        private const string FORMAT_SALVARE = "FormatSalvare";
+        private const string NUME_FISIER = "NumeFisier";
+        public static IStocareData GetAdministratorStocare()
+        {
+            var formatSalvare = ConfigurationManager.AppSettings[FORMAT_SALVARE];
+            var numeFisier = ConfigurationManager.AppSettings[NUME_FISIER];
+            if (formatSalvare != null)
+            {
+                switch (formatSalvare)
+                {
+                    default:
+                    case "bin":
+                        return new AdministrareClienti_FisierBinar(numeFisier + "." + formatSalvare);
+                    case "txt":
+                        return new AdministrareClienti_FisierText(numeFisier + "." + formatSalvare);
+                }
+            }
+
+            return null;
+        }
+    }
+}
